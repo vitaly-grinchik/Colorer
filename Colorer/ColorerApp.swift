adf86f61997d86d608a0fdefcaa1aa6e70d6bc8b
Vitaly Grinchik <vitaly.grinchik@gmail.com>
2023-06-22 15:26:31 +0300

    Initial Commit

---
+++ Colorer/ColorerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorerApp.swift
+//  Colorer
+//
+//  Created by Виталий Гринчик on 22.06.23.
+//
+
+import SwiftUI
+
+@main
+struct ColorerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
